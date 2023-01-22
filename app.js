66571680df1ac0f412ca956b2cd78daa698f6f88
Maria Tharwat <mariatharwat25@gmail.com>
2023-01-23 01:45:34 +0200

    adding mongodb

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -16,14 +17,32 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts=[];
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1/BlogDB',{useNewUrlParser: true});
+  }
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Post = new mongoose.model("Post", postSchema);
 
 app.get("/",function(req,res){
 
-  res.render("home",{
-    startingContent : homeStartingContent,
-     newPost: posts
-     });
+  Post.find({}, function(err, items){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("home",{
+          startingContent : homeStartingContent,
+          newPost: items
+      });
+    }
+  });
+  
 
 }); 
 
@@ -46,25 +65,36 @@ app.get("/compose",function(req,res){
 });
 
 app.post("/compose",function(req,res){
-  const post= {
-    postTitle: req.body.newTitle,
-    postBody: req.body.newPost
-  };
+  const post= new Post({
+    title: req.body.newTitle,
+    content: req.body.newPost
+  });
 
-  posts.push(post);
-  res.redirect("/");
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  });
  
 });
 
-app.get("/posts/:topic",function(req,res){
+app.get("/posts/:id",function(req,res){
 
-  const requestedTitle = req.params.topic;
+  const requestedID = req.params.id;
 
-  posts.forEach(function(post){
-    if(_.lowerCase(requestedTitle) === _.lowerCase(post.postTitle)){
-      res.render("post",{title : post.postTitle , body : post.postBody })
+  Post.findOne({_id : requestedID}, function(err,post){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("post", {title : post.title , body : post.content }) 
     }
-  })
+  });
+
+  // posts.forEach(function(post){
+  //   if(requestedTitle === _.lowerCase(post.postTitle)){
+  //     res.render("post",{title : post.postTitle , body : post.postBody })
+  //   }
+  // })
   
   
 }); 
